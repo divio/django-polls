b89f59b933113b82c49062830912c42a8fc15c77
Daniele Procida <daniele@vurt.org>
2018-06-06 17:27:05 +0100

    Explicitly added application namespace to urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 
 from . import views
 
-
+app_name = "polls"
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
