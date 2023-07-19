a9011524db6ea0031532b3ce9dea35685144f51f
Jofil Inoc Baring <jofil.baring@gmail.com>
2023-07-19 16:08:01 +0800

    fix: django 4.* compatibility

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,12 @@
 #-*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = "polls"
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    re_path(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    re_path(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 ]
